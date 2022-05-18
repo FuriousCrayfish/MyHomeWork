d151bce95c6c520d11508bb1869bc3930f145e6a
Anton Buryka <ton68.92.92@mail.ru>
2022-05-18 21:56:51 +0300

    Выполнил дз, все кроме последнего задания

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
         HomeWork2.arrayChange();
 
+        HomeWork2.arrayCompletion();
+
+        HomeWork2.arrayMultiplication();
+
+        HomeWork2.arrayDiagonal();
+
+        HomeWork2.arrayMaxMin();
+
+        int[] array = new int[]{1, 1, 1, 2, 1};
+
+
+        System.out.println(HomeWork2.checkBalanse(array));
+
     }
 
 }
